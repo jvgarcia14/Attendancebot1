614de9f60f4ec80a6ca3115360abb22b1beea02b
jvgarcia14 <60375104+jvgarcia14@users.noreply.github.com>
2026-02-10 12:11:25 +0800

    Update clockinbot.py

--- clockinbot.py
+++ clockinbot.py
@@ -153,6 +153,7 @@ RAW_PAGES = {
     "fanslyteam1": "Fansly Team1",
     "fanslyteam2": "Fansly Team2",
     "fanslyteam3": "Fansly Team3",
+    "fanslyteam4": "Fansly Team4",
     "francescapaid": "Francesca Paid",
     "gracefree": "Grace Free",
     "haileywfree": "Hailey W Free",
@@ -647,6 +648,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
